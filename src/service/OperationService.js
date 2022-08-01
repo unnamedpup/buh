bc090e2cedf73ad1a65672dd267dcd65d5ecb82c
idevtier <idevtier@gmail.com>
2022-08-01 16:50:56 +0200

    Adding create option method service

--- src/service/OperationService.js
+++ src/service/OperationService.js
@@ -1,14 +1,22 @@
 export class AxiosOperationService {
 
-    async createOperation (operation) {
-
-    }
-
-    async getOperations (limit= 10, pageNumber = 1) {
-        return [
+    constructor() {
+        this.cache = [
             {id: 1, date: '01.01.2022', check: 'Прораб', type: 'Расход', category: 'Питание', desc: 'try', sum: 100},
             {id: 2, date: '01.01.2022', check: 'Прораб', type: 'Расход', category: 'Питание', desc: 'try', sum: 100},
             {id: 3, date: '01.01.2022', check: 'Прораб', type: 'Расход', category: 'Питание', desc: 'try', sum: 100},
         ];
+    }
+
+    async createOperation (operation) {
+      console.log('operation:', operation);
+      operation.id = this.cache.length + 1;
+      console.log('operation:', operation);
+      this.cache.push(operation);
+      return operation;
+    }
+
+    async getOperations (limit= 10, pageNumber = 1) {
+      return this.cache.slice();
     }
 }
